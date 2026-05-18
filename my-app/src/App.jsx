bbc3015964bd33626e0402155214a721fc8ff7be
louiseaholter <holterlouise@gmail.com>
2026-05-18 14:43:18 +0200

    Link till bootsrap i index.html Skapat container stuktur för projeketet i App.jsx med Bootstrap React Andra försöket

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+//Imports
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+//Byggt med Bootstrap
+const App =() => {
+   return (
+       <Container>
+           {/* Rad 1 */}
+         <Row>
+           <Col>
+               <h1> Midnight Sun Dreamhouse</h1>
+           </Col>
+         </Row>
+       </Container>
+     );
+}
