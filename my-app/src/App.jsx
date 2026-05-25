37f75cb10a02e48af067f44e7764401a9cec0de2
NikitaN04 <nikitanilsson04@hotmail.com>
2026-05-25 16:14:16 +0200

    uppdaterat import react router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Routes, Route } from "react-router";
 
 
 //Byggt med Bootstrap
