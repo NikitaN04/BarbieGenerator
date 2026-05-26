90aa8242dafd9d3143bec7e1bffc9dfdb14bbdc7
babblex <szucs_alexandra@hotmail.com>
2026-05-27 00:33:40 +0200

    -importerat link från route-react och lagt till runt button så användaren skickatsvidare till generator sidan vid knapptryck.

--- my-app/src/components/HeroSection/HeroSection.jsx
+++ my-app/src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,7 @@
 import './HeroSection.css';
 import Button from 'react-bootstrap/Button';
+{/*Denna gör så vi kan länka vidare en användare vid knapptryck*/}
+import { Link } from "react-router";
 
 function HeroSection() {
     return (
@@ -9,7 +11,9 @@ function HeroSection() {
               <p>Tell us about your (or your imagined) lifestyle, hobbies, favorite colors, and dreams.</p>
               <p>Then watch the magic happen as we transform your answers into a unique persona and a Dreamhouse created just for you.</p>
               <p>Ready to create a little magic?</p>
+              <Link to="/generator">
               <Button size="lg" className="glow-on-hover">LET'S GET STARTED</Button>
+              </Link>
         </div>
     )
 }
