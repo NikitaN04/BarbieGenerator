7da7b2fb7cc1450787e03af8c78d994f592a00df
babblex <szucs_alexandra@hotmail.com>
2026-05-27 14:27:06 +0200

    Bug-FiX: breadcrumbs renderade nestlade <a>. pga navlink i breadcrumb.item. löste det: Bootstrap skickar nu vidare props (linkProps) till navlink (linkAs), för att bootstrap inte ska rendera sin egen <a> utan renderar en NavLink istället

--- my-app/src/components/BreadCrumbs/BreadCrumbs.jsx
+++ my-app/src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -5,18 +5,31 @@ import { NavLink } from "react-router";
 function BreadCrumbs () {
     return (
      <Breadcrumb>
-        <Breadcrumb.Item>
-            {/*end gör så att det matchar exakt på "/" och inte förvirras med de andra path ex: /gallery. */}
-            <NavLink to="/" end>Start</NavLink>
+     {/*BugFIX: Bootstrap skickar vidare props (linkProps) till navlink  (linkAs), för att bootstrap inte ska rendera sin egen <a> utan renderar en NavLink istället*/}
+        {/*end gör så att det matchar exakt på "/" och inte förvirras med de andra path ex: /gallery.*/}
+        <Breadcrumb.Item 
+            linkAs={NavLink}
+            linkProps={{to: "/", end:true}}
+        >
+            Start
         </Breadcrumb.Item>
-        <Breadcrumb.Item>
-            <NavLink to="/generator">Generator</NavLink>
+        <Breadcrumb.Item
+            linkAs={NavLink}
+            linkProps={{to: "/generator"}}
+        >
+            Generator
         </Breadcrumb.Item>
-        <Breadcrumb.Item>
-            <NavLink to="/gallery">Gallery</NavLink>            
+        <Breadcrumb.Item
+            linkAs={NavLink}
+            linkProps={{to: "/gallery"}}
+        >
+            Gallery        
         </Breadcrumb.Item>
-        <Breadcrumb.Item>
-            <NavLink to="/about-us">About us</NavLink> 
+        <Breadcrumb.Item
+            linkAs={NavLink}
+            linkProps={{to: "/about-us"}}
+        >
+            About us
         </Breadcrumb.Item>
      </Breadcrumb>        
     )
