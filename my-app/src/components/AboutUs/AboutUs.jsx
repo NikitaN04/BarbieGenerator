f03730fdbe354e331d96450c3a094fcb7568633d
louiseaholter <holterlouise@gmail.com>
2026-05-30 15:11:02 +0200

    Lagt till components till about us sidan. Lagt till skelett på hur våran about us kan se ut.

---
+++ my-app/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,28 @@
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-container">
+      <div className="about-card">
+        <h1>About MidnightSun Dreamhouse</h1>
+
+        <p>
+          Lite text om projektet och lite om oss, vem vi är osv.
+        </p>
+
+        <h2>EN APi här</h2>
+        <p>
+          xxxx
+        </p>
+
+        <h2>En APi här</h2>
+        <p>
+          xxxxxx
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
