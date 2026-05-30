20c018294dcff0e77fa66ebfbbf0b4e1c802b794
louiseaholter <holterlouise@gmail.com>
2026-05-30 14:52:31 +0200

    Skapade components till gallery sidan. Använder oss av reactbits gallery circular. Laddade ner npm install ogl för att få få galleriet att fungera som det ska med hjälp av koden från react bits.

---
+++ my-app/src/components/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,30 @@
+import GalleryCircular from '../GalleryCircular/GalleryCircular';
+import './GalleryPage.css';
+
+function GalleryPage() {
+
+    //tillfällig tills andra branchen är klar men bildgenerationen
+    const items = [
+        {
+        image: '/images/test1.jpg',
+        text: 'Test image 1'
+        },
+        {
+        image: '/images/test2.jpg',
+        text: 'Test image 2'
+        }
+    ];
+
+    return (
+      <div className="gallery-page">
+        <GalleryCircular
+            items={items}
+            bend={-1}
+            borderRadius={0.1}
+            scrollSpeed={1.5}
+        />
+      </div>
+    );
+  }
+  
+  export default GalleryPage;
